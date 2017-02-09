89bf6ee263599917a3546797aff5f05df2eddc23
SivanHuang <siyunhuang777@gmail.com>
2017-02-09 19:43:39 +0800

    token revise 3

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	VERIFY_TOKEN: 'sivansivansivan',
+	VERIFY_TOKEN: 'sivansivan',
 	PAGE_TOKEN: 'EAAOPue2B4z4BAMzZA9isR1IXsh6dbZBtSKm4wJIJqBNPgl5a5VsZA6EfrmZAQcxTVJuqwUlH0JtsxvzsG9sEwvzcHH3eHTV1Eom1ZAcZBuyi6S8ZAeiigPrErAdVffxNtYLS5db8DNZC3uVff9TqiTdSrl0pMSCxE7Qda9QbSCvbvYh83QAucpdr'
 }
 npm run build
