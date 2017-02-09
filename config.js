eb0d9104a6a8767037fd0628726c1249e730deb1
SivanHuang <siyunhuang777@gmail.com>
2017-02-09 18:05:21 +0800

    toke revise

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ module.exports = {
 }
 npm run build
 node bin/fb-bot
-curl -ik -X POST "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=<token>"
+curl -ik -X POST "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=<EAAOPue2B4z4BAMzZA9isR1IXsh6dbZBtSKm4wJIJqBNPgl5a5VsZA6EfrmZAQcxTVJuqwUlH0JtsxvzsG9sEwvzcHH3eHTV1Eom1ZAcZBuyi6S8ZAeiigPrErAdVffxNtYLS5db8DNZC3uVff9TqiTdSrl0pMSCxE7Qda9QbSCvbvYh83QAucpdr>"
