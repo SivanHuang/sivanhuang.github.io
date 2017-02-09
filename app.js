c985bec5737dca6a4e4441a655a9e583e85ee218
SivanHuang <siyunhuang777@gmail.com>
2017-02-09 20:06:49 +0800

    token r5

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === <sivansivan>) {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);          
-  }  
-});
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === 'sivansivan') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
