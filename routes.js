1ef0d6ea15f1565c9b09416ebc801aff9050ac87
SivanHuang <siyunhuang777@gmail.com>
2017-02-09 18:59:01 +0800

    route revise

--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
-app.get('/webhook/', function (req, res) {
-  if (req.query['hub.verify_token'] === '<validation_token>') {
+app.get('/https://www.sivanhuang.com/', function (req, res) {
+  if (req.query['hub.verify_token'] === '<sivansivan>') {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
